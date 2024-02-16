5f3fa31d7f8ae14b9e3c1c3f6416c0d53eb9c9d1
wanjala stephen <stephenwanjala145@gmail.com>
2024-02-16 14:46:37 +0300

    update signals

--- inventory/signals.py
+++ inventory/signals.py
@@ -1,5 +1,7 @@
 # inventory/signals.py
 from django.core.mail import send_mail
+from django.db.models import F
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
@@ -7,53 +9,42 @@ from django.utils import timezone
 from django.utils.html import strip_tags
 from notifications.signals import notify
 
-from .models import Purchase, Stock, Sale
+from inventory.models import Purchase, Stock, Sale
 
 
 @receiver(post_save, sender=Purchase)
 def handle_purchase(sender, instance, created, **kwargs):
-    # Update stock level
-    stock, created = Stock.objects.get_or_create(product=instance.product)
-    stock.quantity += instance.quantity
-    stock.save()
+    with transaction.atomic():
+        stock, _ = Stock.objects.get_or_create(product=instance.product)
+        stock.quantity = F('quantity') + instance.quantity
+        stock.save()
 
-    # Send notification to Django notifications
-    if instance.expiration_date <= timezone.now().date() + timezone.timedelta(weeks=1):
-        notify.send(instance.product, recipient=instance.product.responsible_user,
-                    verb='Expiry Notification: Dispose of',
-                    description=f'{instance.product.name} is expiring soon and should be disposed of.')
-
-        # Send email notification
-        send_email_notification(instance.product.responsible_user.email, 'Expiry Notification',
-                                f'{instance.product.name} is expiring soon and should be disposed of.')
+        if instance.expiration_date <= timezone.now().date() + timezone.timedelta(weeks=1):
+            notify.send(instance.product, recipient=instance.product.responsible_user,
+                        verb='Expiry Notification: Dispose of',
+                        description=f'{instance.product.name} is expiring soon and should be disposed of.')
 
 
 @receiver(post_save, sender=Sale)
 def handle_sale(sender, instance, created, **kwargs):
-    # Update stock level after a sale
-    stock, created = Stock.objects.get_or_create(product=instance.product)
-    stock.quantity -= instance.quantity
-    stock.save()
+    with transaction.atomic():
+        stock, _ = Stock.objects.get_or_create(product=instance.product)
+        stock.quantity = F('quantity') - instance.quantity
+        stock.save()
 
-    # Check and notify if stock is low after the sale
-    if stock.is_low_stock():
-        notify.send(stock, recipient=instance.product.responsible_user, verb='Low Stock Notification',
-                    description=f'The stock of {instance.product.name} is low. Please order more.')
+        if stock.is_low_stock():
+            notify.send(stock, recipient=instance.product.responsible_user, verb='Low Stock Notification',
+                        description=f'The stock of {instance.product.name} is low. Please order more.')
 
-        # Send email notification to responsible user
-        send_email_notification(instance.product.responsible_user.email, 'Low Stock Notification',
-                                f'The stock of {instance.product.name} is low. Please order more.')
+            # Send email notification to responsible user
+            send_email_notification(instance.product.responsible_user.email, 'Low Stock Notification',
+                                    f'The stock of {instance.product.name} is low. Please order more.')
 
-        # Send email notification to supplier
-        # Get the latest purchase of the product and its supplier
-        latest_purchase = instance.product.purchase_set.order_by('-purchase_date').first()
-        if latest_purchase:
-            send_email_notification(latest_purchase.supplier.email, 'Low Stock Notification',
-                                    f'The stock of {instance.product.name} is low. Please supply more.')
-        else:
-            # Handle the case when there is no purchase record for the product
-            # For example, you can log an error or send a notification to the admin
-            pass
+            # Send email notification to supplier
+            latest_purchase = instance.product.purchase_set.order_by('-purchase_date').first()
+            if latest_purchase:
+                send_email_notification(latest_purchase.supplier.email, 'Low Stock Notification',
+                                        f'The stock of {instance.product.name} is low. Please supply more.')
 
 
 def send_email_notification(recipient_email, subject, message):
